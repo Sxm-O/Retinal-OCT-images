2730fa9a12395ed937e339507b0e0cb9c655e116
som som <167107017+Sxm-O@users.noreply.github.com>
2025-02-03 12:02:56 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -7,7 +7,8 @@ from PIL import Image
 import streamlit as st
 
 image = Image.open('Khon_Kaen_Wittayayon_School_Logo.svg.png')
-st.image(image, use_column_width=True)
+image = image.resize((200, 200))  
+st.image(image)
 
 #set title
 st.markdown("<h1 style='text-align: center;'>Retinal OCT Image Classification</h1>", unsafe_allow_html=True)
